3d77dcb0f046ed52fdd79147e2f0407d9b412de7
Ben Veghte <BenVeghte@live.com>
2020-08-09 16:28:22 -0700

    Update execution scripts

--- morning.sh
+++ morning.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-. /home/BenVeghte/pre-docker-storage/digital-picture-frame/
+touch itexecuted.txt
 git pull &
 sleep 15s
 chmod -x morning.sh
 chmod -x night.sh
+
+docker image rm digitalpictureframeserver &
+docker image prune &
+sleep 5s
+
 docker build -t digitalpictureframeserver . &
 sleep 30s
 
 docker run --rm -p 3000:3000 -v /home/BenVeghte/Ansel:/NAS --name picture-frame-server digitalpictureframeserver &
 sleep 15s
 
-xset s noblank
-xset dpms force on
 /usr/bin/chromium --noerrdialogs --disable-infobars --kiosk http://localhost:3000 &
+xset dpms force on
+
+
--- night.sh
+++ night.sh
@@ -3,5 +3,5 @@ docker container stop picture-frame-server &
 sleep 15s
 pkill chromium &
 mkdir /home/BenVeghte/thisworked
-xset s blank
+
 xset dpms force off
