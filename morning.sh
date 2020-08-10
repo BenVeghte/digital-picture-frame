48bb758d63194465a16fc07c829858d48955b404
Ben Veghte <BenVeghte@live.com>
2020-08-09 21:07:00 -0700

    Update morning.sh
    wait was preventing chromium from launching

--- morning.sh
+++ morning.sh
@@ -12,8 +12,7 @@ wait
 docker build -t digitalpictureframeserver . 
 wait
 
-docker run --rm -p 3000:3000 -v /home/BenVeghte/Ansel:/NAS --name picture-frame-server digitalpictureframeserver 
-wait
+docker run --rm -p 3000:3000 -v /home/BenVeghte/Ansel:/NAS --name picture-frame-server digitalpictureframeserver &
 export DISPLAY=:0
 xset s noblank
 wait
