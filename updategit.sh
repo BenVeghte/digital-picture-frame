9875ce22af5dfdc3147e41be84e1efd9398b289b
BenVeghte <Benveghte@live.com>
2021-12-26 11:21:29 -0800

    Transition to python and flask instead of node.js

--- updategit.sh
+++ updategit.sh
@@ -11,7 +11,7 @@ wait
 docker build -t digitalpictureframeserver . 
 wait
 
-docker run --restart on-failure -p 3000:3000 -v /home/BenVeghte/Ansel:/NAS -v /home/BenVeghte/docker/digital_picture_frame/goodbadimgs:/goodbadimgs --name picture-frame-server digitalpictureframeserver &
+docker run --restart on-failure -p 8080:8080 -v /home/BenVeghte/Ansel:/NAS -v /home/BenVeghte/docker/digital_picture_frame/goodbadimgs:/goodbadimgs --name picture-frame-server digitalpictureframeserver &
 wait
 
 docker container stop picture-frame-server
